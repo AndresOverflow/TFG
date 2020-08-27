3d83654f832f0d20a3c35e910c24783645654706
Andres Ramos Segui <andresoverflow@gmail.com>
2020-08-27 12:04:09 +0200

    asdf

---
+++ DE_RAM_CEC14/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+
+using namespace std;
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    int x  = 5;
+    int y = 7;
+    cout << "asdfads" << y;
+    return 0;
+
+}
